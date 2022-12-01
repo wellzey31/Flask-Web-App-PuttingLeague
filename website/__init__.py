85a80255c51e28867f35ac83b69eec5ce12a1dd7
brett <brettwwells@gmail.com>
2022-12-01 12:15:27 -0700

    broke the app modifyying it to use postgressql db

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DB_URL = os.getenv('DB_URL')
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,8 +14,8 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    #app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
     db.init_app(app)
 
     from .views import views
